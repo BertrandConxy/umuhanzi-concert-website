9d3e3a518b285984b8b9bb8e328b52a74366ad70
BertrandConxy <90222110+BertrandConxy@users.noreply.github.com>
2022-01-05 17:59:52 +0200

    corrected angle down icon not appearing on github page

--- main.js
+++ main.js
@@ -141,7 +141,7 @@ const moreButton = document.querySelector('.btn-more');
 const moreArtists = document.querySelector('.more-text');
 moreButton.addEventListener('click', () => {
   moreArtists.classList.toggle('show');
-  if (moreButton.innerHTML === 'MORE <img src="/images/angle-down-solid.svg" alt=" ">') {
+  if (moreButton.innerHTML === 'MORE <img src="./images/angle-down-solid.svg" alt=" ">') {
     moreButton.innerHTML = 'LESS <img src="./images/angle-up-solid.svg" alt=" ">';
   } else {
     moreButton.innerHTML = 'MORE <img src="./images/angle-down-solid.svg" alt=" ">';
